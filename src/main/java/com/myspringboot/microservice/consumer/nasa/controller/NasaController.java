d76173ea1f393fa0682ebe0e22a4e3878bbdfe5e
Vinod Kumar Nair <vinod.kumar13@sap.com>
2018-06-01 14:07:37 +0530

    updated pom.xml

--- src/main/java/com/myspringboot/microservice/consumer/nasa/controller/NasaController.java
+++ src/main/java/com/myspringboot/microservice/consumer/nasa/controller/NasaController.java
@@ -3,11 +3,22 @@
  */
 package com.myspringboot.microservice.consumer.nasa.controller;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.actuate.trace.http.HttpTrace.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,7 +38,7 @@ import com.myspringboot.microservice.bean.NasaResponseBean;
 public class NasaController {
 	
 	private Logger logger = LoggerFactory.getLogger(NasaController.class);
-
+	private final String USER_AGENT = "Mozilla/5.0"; 
 	
 	@RequestMapping(path="/", method = RequestMethod.GET)
 	public String defaultRoute() {
@@ -58,33 +69,60 @@ public class NasaController {
 	
 
 	@GetMapping("/googleapi/search/address/{address}/apiKey/{apiKey}")
-	public GoogleResponseBean getAddressfromGoogle(@PathVariable String address, @PathVariable String apiKey) {
+	public GoogleResponseBean getAddressfromGoogle(@PathVariable String address, @PathVariable String apiKey) throws JSONException {
 		logger.info("Entering getAddressfromGoogle@NasaController");
 		String googleUrl = "https://maps.googleapis.com/maps/api/geocode/json?address="+address+"&key="+apiKey;
-		
-				//String googleUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=Vatika%20Tower&key=AIzaSyDO19jHY9aqcMVHR0SAp3JWDY_gFEXo74E
-				
+		//String googleUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=Vatika%20Tower&key=AIzaSyDO19jHY9aqcMVHR0SAp3JWDY_gFEXo74E
 				
 		Map<String, String> uriVariables = new HashMap<>();
 		uriVariables.put("address", address);
 		uriVariables.put("apiKey", apiKey);
 		
-		System.out.println("googleUrl service url: "+googleUrl);
-		System.out.println("address: "+address);
-		System.out.println("apiKey: "+apiKey);
-		
-		
 		RestTemplate restTemplate = new RestTemplate();
-		
 		ResponseEntity<GoogleResponseBean> responseEntity = restTemplate.getForEntity(googleUrl, GoogleResponseBean.class, uriVariables);
-		System.out.println("Google responseEntity->>>>>>>>: "+responseEntity);
-		
 		GoogleResponseBean response = responseEntity.getBody();
-		
-		
-		System.out.println("Google response: "+response);
+		System.out.println("response ->"+response.getResults());
 		
 		logger.info("Exiting getAddressfromGoogle@NasaController");
 		return new GoogleResponseBean(response.getResults(), null, null);
 	}
+	
+	
+	@GetMapping("/locationapi/search/address/{address}/apiKey/{apiKey}")
+	public String getLocation(@PathVariable String address, @PathVariable String apiKey) throws Exception{
+		logger.info("Entering getAddressfromGoogle@NasaController");
+		String googleUrl = "https://demo0954903.mockable.io/location";
+				
+		String url = "https://demo7704768.mockable.io/orderstatus";
+        
+		
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // optional default is GET
+        con.setRequestMethod("GET");
+
+        //add request header
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+              response.append(inputLine);
+        }
+        in.close();
+        JSONObject json = new JSONObject(response.toString()); 
+        String msg = json.get("location").toString();
+
+       
+        return msg;
+
+ }
 }
